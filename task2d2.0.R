399166b48eceea2518a4432600f8861fd6f736b9
Yanjiao Yang <yanjiao.yang@duke.edu>
2022-11-28 14:08:53 -0500

    fix

--- task2d2.0.R
+++ task2d2.0.R
@@ -1,8 +1,11 @@
-## split data first
-## input training data that has been split--with fold
+library(MASS)
+library(class)
 
+# Split the data first
+# first method: systematic assignment
 systematic.split = function(image, i){
-  # divide the image into 2T*2T pieces
+  
+  # divide the image into 2T*2T pieces (here T=4)
   T = 4
   image = image %>%
     mutate(level.x = as.factor(cut(image$x, breaks = 2*T)),
@@ -17,24 +20,28 @@ systematic.split = function(image, i){
   
   return(image %>% select(-c(level.x, level.y, mod)))
 }
+
 imagem1.syst = systematic.split(imagem1, i=1)
 imagem2.syst = systematic.split(imagem2, i=2)
 imagem3.syst = systematic.split(imagem3, i=3)
-# combine the three images
+
 image.syst = rbind(imagem1.syst, imagem2.syst, imagem3.syst)
-# create the test data
+
+# create the test data for the first split
 image.syst.test = NULL
 set.seed(111111)
 idx.test = c()
+
 for (i in 1:2){
   idx = sample(1:length(unique(image.syst$fold)), size = 1)
   image.syst.test = rbind(image.syst.test, image.syst[image.syst$fold==idx, ])
   idx.test = c(idx.test, idx)
 }
-# create training data
+
+# create training data for the first split
 image.syst.train = image.syst[!(image.syst$fold %in% idx.test), ]
 
-
+# second method: create a buffer
 buf.split = function(image, width, i){
   xcut = quantile(image$x, c(4/9, 5/9, 1))
   ycut = quantile(image$y, c(4/9, 5/9, 1))
@@ -64,26 +71,32 @@ buf.split = function(image, width, i){
     )
   return (image %>% select(-c(level.x, level.y)))
 }
+
 imagem1.buf = buf.split(imagem1, width = 3, i = 1)
 imagem2.buf = buf.split(imagem2, width = 3, i = 2)
 imagem3.buf = buf.split(imagem3, width = 3, i = 3)
+
+# create test data for the second split
 image.buf.test = rbind(imagem1.buf[imagem1.buf$fold==0,],
                        imagem2.buf[imagem2.buf$fold==0,],
                        imagem3.buf[imagem3.buf$fold==0,]) %>%
   select(-fold) 
 
-# create training data
+# create training data for the second split
 image.buf.train = rbind(imagem1.buf[!(imagem1.buf$fold %in% c(0,-1)), ],
                         imagem2.buf[!(imagem2.buf$fold %in% c(0,-1)), ],
                         imagem3.buf[!(imagem3.buf$fold %in% c(0,-1)), ])
 
 
-CVmaster2=function(classifier,xtrain,ytrain,K,loss){
-  ## set up
+# CVmaster return the K-fold CV loss on the training set 
+CVmaster = function(classifier, xtrain, ytrain, K, loss){
+  
+  # set up
   classifiers=c("logistic","LDA","QDA","Naive Bayes","knn")
   losses=c("accuracy")
-  if(!(classifier%in%classifiers)){
-    print("Please choose split classifiers: logistic, LDA, QDA, Naive Bayes, knn.")
+  
+  if(!(classifier %in% classifiers)){
+    print("Please choose classifiers from logistic, LDA, QDA, Naive Bayes, knn.")
     break
   }
   if(!(loss%in%losses)){
@@ -91,55 +104,80 @@ CVmaster2=function(classifier,xtrain,ytrain,K,loss){
     break
   }
   
-  df=data.frame(xtrain,label=ytrain)
-  data=df%>%filter(label!=0)
-  data$label[data$label==-1]=0
-  fold=unique(data$fold)
-  fold.sample=sample(fold)
+  df = data.frame(xtrain, label = ytrain)
+  data = df %>% 
+    filter(label != 0)
+  data$label[data$label == -1] = 0
+  fold = unique(data$fold)
+  fold.sample = sample(fold)
   
-  ## matrix to save result
-  cvresult=matrix(NA,ncol=2,nrow=K)
-  colnames(cvresult)=c("fold","acc")
+  # store the result
+  cvresult = matrix(NA, ncol = 2, nrow = K)
+  colnames(cvresult) = c("fold","acc")
   
-  ## K-folds
   for(i in 1:K){
-    datatrain=data%>%filter(fold!=fold.sample[i%%K+1])
-    dataval=data%>%filter(fold==fold.sample[i%%K+1])
-    ## classifiers
-    if(classifier=="logistic"){
-      glm.fit=glm(label~NDAI+SD+CORR+DF+CF+BF+AF+AN,data=datatrain,family=binomial)
-      glm.probs=predict(glm.fit,dataval,type="response")
-      glm.pred=rep(0,length(glm.probs))
-      glm.pred[glm.probs>0.5]=1
-      pred=glm.pred
+    
+    # training set
+    datatrain = data %>% 
+      filter(fold != fold.sample[i %% length(fold) + 1])
+    
+    # validation set
+    dataval = data %>%
+      filter(fold == fold.sample[i %% length(fold) + 1])
+    
+    # classifiers
+    model_formula = as.formula("label~NDAI+SD+CORR+DF+CF+BF+AF+AN")
+    
+    # logistic regression
+    if(classifier == "logistic"){
+      glm.fit = glm(model_formula, data = datatrain, family = binomial)
+      glm.probs = predict(glm.fit, dataval, type="response")
+      glm.pred = rep(0, length(glm.probs))
+      glm.pred[glm.probs > 0.5] = 1
+      pred = glm.pred
     }
-    if(classifier=="LDA"){
-      lda.fit=lda(label~NDAI+SD+CORR+DF+CF+BF+AF+AN,data=datatrain)
-      lda.pred=predict(lda.fit,dataval)
-      pred=lda.pred$class
+    
+    # LDA
+    if(classifier == "LDA"){
+      lda.fit = lda(model_formula, data = datatrain)
+      lda.pred = predict(lda.fit, dataval)
+      pred = lda.pred$class
     }
-    if(classifier=="QDA"){
-      qda.fit=qda(label~NDAI+SD+CORR+DF+CF+BF+AF+AN,data=datatrain)
-      qda.pred=predict(qda.fit,dataval)
-      pred=qda.pred$class
+    
+    # QDA
+    if(classifier == "QDA"){
+      qda.fit = qda(model_formula, data = datatrain)
+      qda.pred = predict(qda.fit, dataval)
+      pred = qda.pred$class
     }
-    if(classifier=="Naive Bayes"){
-      nb.fit=naiveBayes(label~NDAI+SD+CORR+DF+CF+BF+AF+AN,data=datatrain)
-      nb.pred=predict(nb.fit,dataval)
-      pred=nb.pred
+    
+    # Naive Bayes
+    if(classifier == "Naive Bayes"){
+      nb.fit = naiveBayes(model_formula, data = datatrain)
+      nb.pred = predict(nb.fit, dataval)
+      pred = nb.pred
     }
-    if(classifier=="knn"){
-      knn.pred=knn(datatrain[,4:11],dataval[,4:11],datatrain$label,k=5)
-      pred=knn.pred
+    
+    # KNN
+    if(classifier == "knn"){
+      train.X = datatrain[, c("NDAI", "SD", "CORR", "DF", "CF", "BF", "AF", "AN")]
+      val.X = dataval[, c("NDAI", "SD", "CORR", "DF", "CF", "BF", "AF", "AN")]
+      knn.pred = knn(train.X, val.X, datatrain$label, k=5)
+      pred = knn.pred
     }
-    ## loss functions
-    if(loss=="accuracy"){
-      acc=mean(pred==dataval$label)
+    
+    # loss functions
+    if(loss == "accuracy"){
+      acc = mean(pred == dataval$label)
     }
-    ## save results
-    cvresult[i,1]=i
-    cvresult[i,2]=acc
+    
+    # save results
+    cvresult[i, 1] = i
+    cvresult[i, 2] = acc
   }
   
-  return(cvresult)
+  return(as.data.frame(cvresult))
+  
 }
+
+a = CVmaster(classifier = "LDA", xtrain = image.syst.train, ytrain = image.syst.train$label, K=12, loss="accuracy")
